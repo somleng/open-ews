7226c6a9b246f358052870603023211086cb9765
Samnang Chhun <samnang.chhun@gmail.com>
2024-12-17 16:43:35 +0700

    Add more tests for request schema

--- spec/request_schemas/v1/beneficiary_request_schema_spec.rb
+++ spec/request_schemas/v1/beneficiary_request_schema_spec.rb
@@ -5,7 +5,10 @@ module V1
     it "validates the msisdn" do
       contact = create(:contact)
 
-      expect(validate_schema(input_params: { data: { attributes: { msisdn: nil } } })).not_to have_valid_field(:data, :attributes, :msisdn)
+      expect(
+        validate_schema(input_params: { data: { attributes: { msisdn: nil } } })
+      ).not_to have_valid_field(:data, :attributes, :msisdn)
+
       expect(
         validate_schema(input_params: { data: { attributes: { msisdn: "+855 97 2345 6789" } }  })
       ).not_to have_valid_field(:data, :attributes, :msisdn)
@@ -27,6 +30,88 @@ module V1
           options: { account: contact.account }
         )
       ).to have_valid_field(:data, :attributes, :msisdn)
+    end
+
+    it "validates the iso_country_code" do
+      expect(
+        validate_schema(input_params: { data: { attributes: { iso_country_code: nil } } })
+      ).not_to have_valid_field(:data, :attributes, :iso_country_code)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { iso_country_code: "foo" } }  })
+      ).not_to have_valid_field(:data, :attributes, :iso_country_code)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { iso_country_code: "kh" } }  })
+      ).to have_valid_field(:data, :attributes, :iso_country_code)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { iso_country_code: "KH" } }  })
+      ).to have_valid_field(:data, :attributes, :iso_country_code)
+    end
+
+    it "validates the language_code" do
+      expect(
+        validate_schema(input_params: { data: { attributes: { language_code: nil } } })
+      ).to have_valid_field(:data, :attributes, :language_code)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: {} }  })
+      ).to have_valid_field(:data, :attributes, :language_code)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { language_code: "km" } }  })
+      ).to have_valid_field(:data, :attributes, :language_code)
+    end
+
+    it "validates the gender" do
+      expect(
+        validate_schema(input_params: { data: { attributes: { gender: nil } } })
+      ).to have_valid_field(:data, :attributes, :gender)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: {} }  })
+      ).to have_valid_field(:data, :attributes, :gender)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { gender: "foo" } } })
+      ).not_to have_valid_field(:data, :attributes, :gender)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { gender: "M" } } })
+      ).to have_valid_field(:data, :attributes, :gender)
+    end
+
+    it "validates the date of birth" do
+      expect(
+        validate_schema(input_params: { data: { attributes: { date_of_birth: nil } } })
+      ).to have_valid_field(:data, :attributes, :date_of_birth)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: {} }  })
+      ).to have_valid_field(:data, :attributes, :date_of_birth)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { date_of_birth: "invalid-date" } } })
+      ).not_to have_valid_field(:data, :attributes, :date_of_birth)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { date_of_birth: "2000-01-01" } } })
+      ).to have_valid_field(:data, :attributes, :date_of_birth)
+    end
+
+    it "validates the address" do
+      expect(
+        validate_schema(input_params: { data: { attributes: { address: nil } } })
+      ).not_to have_valid_field(:data, :attributes, :address)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: {} }  })
+      ).to have_valid_field(:data, :attributes, :address)
+
+      expect(
+        validate_schema(input_params: { data: { attributes: { address: { iso_region_code: "KH-1" } } } })
+      ).to have_valid_field(:data, :attributes, :address, :iso_region_code)
     end
 
     it "validates the metadata fields attributes" do
