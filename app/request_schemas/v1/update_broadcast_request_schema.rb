2e3759d5d0dc78c79200308f6ba5c26a6a50615f
Samnang Chhun <samnang.chhun@gmail.com>
2025-01-29 17:24:59 +0700

    Update state transition

--- app/request_schemas/v1/update_broadcast_request_schema.rb
+++ app/request_schemas/v1/update_broadcast_request_schema.rb
@@ -7,6 +7,7 @@ module V1
         required(:attributes).value(:hash).schema do
           optional(:audio_url).filled(:string)
           optional(:beneficiary_parameters).filled(:hash).schema(BeneficiaryFilter.schema)
+          optional(:status).filled(included_in?: Callout.aasm.states.map { _1.name.to_s })
           optional(:metadata).value(:hash)
         end
       end
@@ -16,6 +17,15 @@ module V1
 
     rule(data: :id) do
       key.failure("Updates are only allowed when the status is pending.") unless resource.updatable?
+    end
+
+    attribute_rule(:status) do
+      break unless key?
+      break if value == "running" && (resource.may_start? || resource.may_resume?)
+      break if value == "paused" && resource.may_pause?
+      break if value == "stopped" && resource.may_stop?
+
+      key.failure("does not allow to transition from #{resource.status} to #{value}")
     end
   end
 end
