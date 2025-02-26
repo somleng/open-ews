8a579b3444dd8f27a0a9274fc5f6ab6a3225bade
Samnang Chhun <samnang.chhun@gmail.com>
2025-02-26 16:13:19 +0700

    Refactor filter classes

--- app/filters/alert_filter.rb
+++ app/filters/alert_filter.rb
@@ -1,23 +1,3 @@
-class AlertFilter < ApplicationFilter
-  params do
-    FieldDefinitions::AlertFields.each do |field|
-      optional(field.name.to_sym).schema(field.schema)
-    end
-  end
-
-  def output
-    result = super
-    return {} if result.blank?
-
-    result.map do |(filter, condition)|
-      operator, value = condition.first
-      beneficiary_field = FieldDefinitions::AlertFields.find(filter.to_s)
-
-      FilterField.new(
-        field_definition: beneficiary_field,
-        operator: operator,
-        value: value
-      )
-    end
-  end
+class AlertFilter < ResourceFilter
+  has_fields FieldDefinitions::AlertFields
 end
--- app/filters/beneficiary_filter.rb
+++ app/filters/beneficiary_filter.rb
@@ -1,23 +1,3 @@
-class BeneficiaryFilter < ApplicationFilter
-  params do
-    FieldDefinitions::BeneficiaryFields.each do |field|
-      optional(field.name.to_sym).schema(field.schema)
-    end
-  end
-
-  def output
-    result = super
-    return {} if result.blank?
-
-    result.map do |(filter, condition)|
-      operator, value = condition.first
-      beneficiary_field = FieldDefinitions::BeneficiaryFields.find(filter.to_s)
-
-      FilterField.new(
-        field_definition: beneficiary_field,
-        operator: operator,
-        value: value
-      )
-    end
-  end
+class BeneficiaryFilter < ResourceFilter
+  has_fields FieldDefinitions::BeneficiaryFields
 end
