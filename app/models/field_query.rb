4afdbe83b4b24d7e52887a14996775ee546dacc8
David Wilkie <dwilkie@gmail.com>
2026-09-10 15:22:37 +0700

    WIP

--- app/models/field_query.rb
+++ app/models/field_query.rb
@@ -1,5 +1,48 @@
-FieldQuery = Data.define(:association, :arel_column) do
+class FieldQuery < Data.define(:association, :arel_column)
   def initialize(**)
     super(association: nil, arel_column: nil, **)
+  end
+
+  def to_arel(**)
+    ArelBuilder.new(arel_column:, **).build
+  end
+
+  class ArelBuilder
+    attr_reader :arel_column, :operator, :value
+
+    def initialize(arel_column:, operator:, value:, **)
+      @arel_column = arel_column
+      @operator = operator.to_sym
+      @value = value
+    end
+
+    def build
+      arel_column.public_send(operator_method, filter_value)
+    end
+
+    private
+
+    # NOTE: cast from user input operator to arel attribute's predications
+    # https://www.rubydoc.info/gems/arel/Arel/Predications
+    def operator_method
+      case operator
+      when :eq, :not_eq, :gt, :gteq, :lt, :lteq, :between, :in, :not_in then operator
+      when :contains, :starts_with then :matches
+      when :not_contains then :does_not_match
+      when :is_null then value ? :eq : :not_eq
+      else
+        raise ArgumentError, "Unsupported operator #{operator}"
+      end
+    end
+
+    def filter_value
+      case operator
+      when :is_null then nil
+      when :contains, :not_contains then Arel::Nodes::Quoted.new("%#{value}%")
+      when :starts_with then Arel::Nodes::Quoted.new("#{value}%")
+      when :between then Range.new(value[0], value[1])
+      else value
+      end
+    end
   end
 end
--- app/models/filter_field.rb
+++ app/models/filter_field.rb
@@ -1,48 +1,15 @@
 FilterField = Data.define(:operator, :value, :query, :metadata) do
-  def initialize(**attributes)
-    super(
-      metadata: {},
-      **attributes,
-      operator: attributes.fetch(:operator).to_sym
-    )
+  def initialize(**)
+    super(metadata: {}, **)
   end
 
   def to_query
-    if query.respond_to?(:to_arel)
-      query.to_arel(operator:, value:, **metadata)
-    else
-      query.arel_column.public_send(operator_method, filter_value)
-    end
+    query.to_arel(operator:, value:, **metadata)
   end
 
   def associations
     return [] if query.association.blank?
 
     [ query.association ]
-  end
-
-  private
-
-  # NOTE: cast from user input operator to arel attribute's predications
-  # https://www.rubydoc.info/gems/arel/Arel/Predications
-  def operator_method
-    case operator
-    when :eq, :not_eq, :gt, :gteq, :lt, :lteq, :between, :in, :not_in then operator
-    when :contains, :starts_with then :matches
-    when :not_contains then :does_not_match
-    when :is_null then value ? :eq : :not_eq
-    else
-      raise ArgumentError, "Unsupported operator #{operator}"
-    end
-  end
-
-  def filter_value
-    case operator
-    when :is_null then nil
-    when :contains, :not_contains then Arel::Nodes::Quoted.new("%#{value}%")
-    when :starts_with then Arel::Nodes::Quoted.new("#{value}%")
-    when :between then Range.new(value[0], value[1])
-    else value
-    end
   end
 end
