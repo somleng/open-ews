83dd6229c4a70c8c7fe112b33af3c975a386fb28
David Wilkie <dwilkie@gmail.com>
2026-09-11 23:10:14 +0700

    WIP

--- app/models/country_locality_data/collection.rb
+++ app/models/country_locality_data/collection.rb
@@ -24,7 +24,6 @@ module CountryLocalityData
       items.sort_by { it.path.size }.each_with_object([]) do |locality, tree|
         node_data = yield(locality)
         nodes_by_path[locality.path] = node_data
-
         parent_node = nodes_by_path[locality.path[0...-1]]
 
         if parent_node.present?
